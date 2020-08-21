73fc2d791ee13e2e007695c261308a955fb4bc13
iamtodor <todor.ilya@gmail.com>
2020-08-21 19:31:17 +0300

    refactor

--- producer/src/main/java/com/iamtodor/Utils.java
+++ producer/src/main/java/com/iamtodor/Utils.java
@@ -1,7 +1,6 @@
 package com.iamtodor;
 
 import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +11,7 @@ public class Utils {
     public static String extractAuthorIdFromTweet(String tweet) {
         String userId = "";
         try {
-            userId = JsonParser
-                    .parseString(tweet)
+            userId = JsonParser.parseString(tweet)
                     .getAsJsonObject()
                     .get("user")
                     .getAsJsonObject()
