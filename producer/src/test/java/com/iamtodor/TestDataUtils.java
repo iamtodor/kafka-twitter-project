b7b36b8f7f102e9bbf18994f0918fc29e28413f2
iamtodor <todor.ilya@gmail.com>
2020-08-21 13:30:49 +0300

    refactor and add tests

---
+++ producer/src/test/java/com/iamtodor/TestDataUtils.java
@@ -0,0 +1,22 @@
+package com.iamtodor;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestDataUtils {
+
+    public static final String EXPECTED_USER_ID = "16259258";
+
+    public static String loadJsonRecord() {
+        byte[] encoded = new byte[0];
+        try {
+            encoded = Files.readAllBytes(Paths.get("src/test/resources/record.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(encoded, StandardCharsets.UTF_8);
+    }
+
+}
